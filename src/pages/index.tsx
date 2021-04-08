da656e3a964a34fa05095c6e977d89dc84db9824
thauandev <thauandepaula@icloud.com>
2021-04-08 09:55:16 -0300

    Title dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Olá</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+    </>
+  );
 }
