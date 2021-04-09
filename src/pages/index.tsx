89dff95750d7f39d538f0d52fd05cbbcea71809d
thauandev <thauandepaula@icloud.com>
2021-04-09 14:44:09 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IeLATAu1bWEU44D0wx6jWYD");
 
   const product = {
@@ -54,5 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
