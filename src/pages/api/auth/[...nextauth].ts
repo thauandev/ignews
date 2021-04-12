16dd2dea2a873f24952ecae1a976efab994adcda
thauandev <thauandepaula@icloud.com>
2021-04-12 14:46:09 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import {query as q} from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import {fauna} from '../../../services/fauna'
 
 export default NextAuth({
  
@@ -9,8 +13,29 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-   
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+    const { email } = user
+      
+    try {
+      await fauna.query(
+      q.Create(
+        q.Collection('users'),
+        { data: { email } }
+      )
+    )
+    return true
+  } catch {
+    return false
+  }
+      
+     
+    },
+  }
 
 
 })
