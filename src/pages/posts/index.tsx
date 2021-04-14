0b6ded1ceb813dcc02275bd1b9ea4fa1cd51f933
thauandev <thauandepaula@icloud.com>
2021-04-14 15:25:13 -0300

    Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,10 @@
-import Head from "next/head";
 import styles from "./styles.module.scss";
+
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
+import Head from "next/head";
+
+import { getPrismicClient } from "../../services/prismic";
 
 export default function Posts() {
   return (
@@ -30,3 +35,21 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(response);
+
+  return {
+    props: {},
+  };
+};
