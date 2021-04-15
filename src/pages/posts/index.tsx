0ec08acab83e3abd8c25222c5ca340d5f42b4c07
thauandev <thauandepaula@icloud.com>
2021-04-15 10:01:49 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,10 +3,22 @@ import styles from "./styles.module.scss";
 import { GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
 import Head from "next/head";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
-export default function Posts() {
+type Posts = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Posts[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,21 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Teste de título</strong>
-            <p>Parágrafo</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Teste de título</strong>
-            <p>Parágrafo</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Teste de título</strong>
-            <p>Parágrafo</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -47,9 +51,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
